35829494ff172973955eddef4a6446679d304e67
Thomas Lamars <thomas.lamars@wearetriple.com>
2024-08-18 15:53:24 +0200

    feat: change blob animation a bit

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,7 +1,7 @@
 <div class="background">
   <div class="blob small-blob blue-blob" />
   <div class="blob small-blob purple-blob" />
-  <div class="blob green-blob" />
+  <div class="blob big-blob green-blob" />
 </div>
 
 <style lang="scss">
@@ -53,9 +53,28 @@
     }
   }
 
+  .big-blob {
+    width: 60rem;
+    height: 50rem;
+    top: -30rem;
+
+    @include phone-only {
+      width: 30rem;
+      height: 55rem;
+      top: -20rem;
+    }
+
+    @include tablet-only {
+      width: 50rem;
+      height: 50rem;
+      top: -25rem;
+    }
+  }
+
   .blue-blob {
     right: 10rem;
     background: var(--blob2);
+    animation: moveSmallBlob 2.5s infinite;
 
     @include phone-only {
       right: -5rem;
@@ -69,6 +88,7 @@
   .purple-blob {
     left: 10rem;
     background: var(--blob3);
+    animation: moveSmallBlob 3.5s infinite;
 
     @include phone-only {
       left: -5rem;
@@ -80,35 +100,23 @@
   }
 
   .green-blob {
-    width: 60rem;
-    height: 60rem;
     left: 50%;
-    margin-left: -34rem;
-    top: -32rem;
+    margin-left: -30rem;
 
     background: var(--blob1);
-
-    display: flex;
-    justify-content: center;
+    animation: moveBigBlob 3s infinite;
 
     @include phone-only {
-      width: 20rem;
-      height: 55rem;
-      margin-left: -10rem;
-      top: -35rem;
-      opacity: 0.75;
+      margin-left: -15rem;
+      top: -40rem;
     }
 
     @include tablet-only {
-      width: 40rem;
-      height: 50rem;
-      margin-left: -20rem;
-      top: -30rem;
+      margin-left: -25rem;
     }
   }
 
-  // animate the blobs in multiple directions, speeds, and sizes in bigger impacts with more steps
-  @keyframes moveBlobGreen {
+  @keyframes moveBigBlob {
     0% {
       transform: translateY(0) scale(1);
     }
@@ -126,25 +134,7 @@
     }
   }
 
-  @keyframes moveBlobBlue {
-    0% {
-      transform: translateY(0) scale(1);
-    }
-    25% {
-      transform: translate(-2rem, 2rem) scale(1.3);
-    }
-    50% {
-      transform: translate(3rem, -5rem) scale(0.9);
-    }
-    75% {
-      transform: translate(-5rem, -3rem) scale(1.1);
-    }
-    100% {
-      transform: translateY(0) scale(1);
-    }
-  }
-
-  @keyframes moveBlobPurple {
+  @keyframes moveSmallBlob {
     0% {
       transform: translateY(0) scale(1);
     }
@@ -160,17 +150,5 @@
     100% {
       transform: translateY(0) scale(1);
     }
-  }
-
-  .blue-blob {
-    animation: moveBlobBlue 2.5s infinite;
-  }
-
-  .purple-blob {
-    animation: moveBlobPurple 3.5s infinite;
-  }
-
-  .green-blob {
-    animation: moveBlobGreen 3s infinite;
   }
 </style>
