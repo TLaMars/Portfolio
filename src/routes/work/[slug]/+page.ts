6cc1fc543de402ed559a29735b99a62361d29e92
Thomas Lamars <thomas.lamars@wearetriple.com>
2025-04-05 14:49:18 +0200

    feat: astro

--- src/routes/work/[slug]/+page.ts
+++
@@ -1,5 +0,0 @@
-import data from "$lib/data/projects.json";
-import type { PageLoad } from "./$types";
-
-export const load: PageLoad = ({ params }) =>
-  data.find((project) => project.slug === params.slug);
