890fa0cdeb031ab92f934c334b809efffb57fce1
Thomas Lamars <thomas.lamars@wearetriple.com>
2026-04-08 21:01:59 +0200

    fix: make the site static so it works with pages again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-
-  adapter: cloudflare({
-    imageService: "passthrough",
-  }),
+  image: {
+    service: { entrypoint: "astro/assets/services/noop" },
+  },
 });
